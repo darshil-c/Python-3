604e2b8f0491b241b62eafe5e7c2ed602944acef
Darshil Chaudhari <darshilc5@gmail.com>
2022-05-08 15:17:14 -0600

    Problem #3 Function with a Boolean check to return different results.

---
+++ Functions/Practice Problems/problem_3.py
@@ -0,0 +1,17 @@
+# Define a function called myfunc that takes in a Boolean value
+# If True, return 'Hello', and if False, return 'Goodbye'
+
+def myfunc(x):
+    if x:
+        return "Hello"
+    else:
+        return "Goodbye"
+
+# Test case with True
+
+print(myfunc(True))
+
+# Test case with False
+
+print(myfunc(False))
+
