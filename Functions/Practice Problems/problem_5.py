a41d8c277576ae746c06b43a27f61a57821868fc
Darshil Chaudhari <darshilc5@gmail.com>
2022-05-08 15:25:55 -0600

    Problem #5 Basic summation of two arguments.

---
+++ Functions/Practice Problems/problem_5.py
@@ -0,0 +1,12 @@
+# Define a function called myfunc that takes in two arguments and returns their sum
+
+def myfunc(x, y):
+    return x + y
+
+# Test case x = 5, y = 5, expected output = 10
+
+print(myfunc(5, 5))
+
+# Test case x = -15, y = 9, expected output = -6
+
+print(myfunc(-15, 9))
